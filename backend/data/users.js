cfa31ecf56d1ec30b705caae9f7366303a5e8640
malish-stha <malish.shrestha@deerwalk.edu.np>
2023-09-18 17:35:06 +0545

    (generated by useblackbox.ai )

---
+++ backend/data/users.js
@@ -0,0 +1,24 @@
+import bcrypt from "bcryptjs";
+
+const users = [
+  {
+    name: "Admin User",
+    email: "admin@email.com",
+    password: bcrypt.hashSync("123450,10"),
+    isAdmin: true,
+  },
+  {
+    name: "John Doe",
+    email: "john@email.com",
+    password: bcrypt.hashSync("123450,10"),
+    isAdmin: false,
+  },
+  {
+    name: "Lmao doe",
+    email: "lmao@email.com",
+    password: bcrypt.hashSync("123450,10"),
+    isAdmin: false,
+  },
+];
+
+export default users;
---
+++ frontend/src/screens/ProductScreen.jsx
@@ -0,0 +1,91 @@
+import React from "react";
+import { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
+import { Link } from "react-router-dom";
+import {
+  Row,
+  Col,
+  Image,
+  ListGroup,
+  Card,
+  Button,
+  ListGroupItem,
+} from "react-bootstrap";
+import Rating from "../components/Rating";
+import axios from "axios";
+
+const ProductScreen = () => {
+  const [product, setProduct] = useState([]);
+
+  const { id: productId } = useParams();
+
+  useEffect(() => {
+    const fetchProduct = async () => {
+      const { data } = await axios.get(`/api/products/${productId}`);
+      setProduct(data);
+    };
+    fetchProduct();
+  }, [productId]);
+  return (
+    <>
+      <Link className="btn btn-light my-3" to="/">
+        GO BACK
+      </Link>
+      <Row>
+        <Col md={5}>
+          <Image src={product.image} alt={product.name} fluid />
+        </Col>
+        <Col md={4}>
+          <ListGroup variant="flush">
+            <ListGroup.Item>
+              <h3>{product.name}</h3>
+            </ListGroup.Item>
+            <ListGroup.Item>
+              <Rating
+                value={product.rating}
+                text={`${product.numReviews} reviews`}
+              ></Rating>
+            </ListGroup.Item>
+            <ListGroupItem> Price: ${product.price}</ListGroupItem>
+            <ListGroupItem>Description: {product.description}</ListGroupItem>
+          </ListGroup>
+        </Col>
+        <Col md={3}>
+          <Card>
+            <ListGroup variant="flush">
+              <ListGroup.Item>
+                <Row>
+                  <Col>Price:</Col>
+                  <Col>
+                    <strong>${product.price}</strong>
+                  </Col>
+                </Row>
+              </ListGroup.Item>
+              <ListGroup.Item>
+                <Row>
+                  <Col>Status:</Col>
+                  <Col>
+                    <strong>
+                      {product.countInStock > 0 ? "In Stock" : "Out of Stock"}
+                    </strong>
+                  </Col>
+                </Row>
+              </ListGroup.Item>
+              <ListGroup.Item>
+                <Button
+                  className="btn-block"
+                  type="button"
+                  disabled={product.countInStock === 0}
+                >
+                  Add To Cart
+                </Button>
+              </ListGroup.Item>
+            </ListGroup>
+          </Card>
+        </Col>
+      </Row>
+    </>
+  );
+};
+
+export default ProductScreen;
